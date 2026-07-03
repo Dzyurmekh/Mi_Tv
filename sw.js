ed6dda87f3d7d62a2c5be3415f4d17b173565ca2
Dzyurmekh <azteckbest@gmail.com>
2026-07-03 23:35:48 +0200

    Actualizar repositorio: mejoras en index.html, manifest.json, package.json, robots.txt, sitemap.xml y sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,18 @@
-const CACHE_NAME = "mitv-v1"
-const urlsToCache = ["/", "/css/style.css", "/js/principal.js", "/js/hls.min.js", "/manifest.json"]
+const CACHE_NAME = "mitv-v3"
+const urlsToCache = ["./", "./css/style.css", "./js/principal.js", "./js/hls.min.js", "./manifest.json"]
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting()
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)))
+})
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  )
 })
 
 self.addEventListener("fetch", (event) => {
